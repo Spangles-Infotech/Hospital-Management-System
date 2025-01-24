91428ffae242d1b9b4b23f4c9513a42da20f26a6
Vaishnavi.V <vaishnavijk04@gmail.com>
2025-01-24 17:04:03 +0530

    Created Prescription table

---
+++ src/utils/variable/prescriptions.js
@@ -0,0 +1,129 @@
+export const tableHeading = [
+  {
+    name: "SI. No.",
+    path: "SIno",
+  },
+  {
+    name: "Date",
+    path: "date",
+  },
+  {
+    name: "Patient ID",
+    path: "patientId",
+  },
+  {
+    name: "Patient Name",
+    path: "patientName",
+  },
+
+  {
+    name: "Phone No.",
+    path: "phoneNo",
+  },
+
+  {
+    name: "Doctor's Name",
+    path: "doctorName",
+  },
+  {
+    name: "No. of. Medicine",
+    path: "NoOfMedicine",
+  },
+
+  {
+    name: "Action",
+    path: "normal",
+  }
+]
+
+export const tableValue = [
+    {
+      id:"123",
+      SIno:"01",
+      date:"24/10/2023",
+      patientId: "323252",
+      patientName: "John Doe",
+      phoneNo: "9087654321",
+      doctorName: "Dr. Suresh",
+      NoOfMedicine: "04",
+      
+    },
+    {
+        id:"124",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"125",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"126",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "908765432",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"127",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"128",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"129",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+      {
+        id:"140",
+        SIno:"01",
+        date:"24/10/2023",
+        patientId: "323252",
+        patientName: "John Doe",
+        phoneNo: "9087654321",
+        doctorName: "Dr. Suresh",
+        NoOfMedicine: "04",
+        
+      },
+    
+]
