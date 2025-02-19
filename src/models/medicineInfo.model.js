a08283baaa5a10fdbcc763d31b8422846b7ed55e
Ajay <jijoajay45@gmail.com>
2025-02-19 11:41:32 +0530

    worked on the medicine modal

--- src/models/medicineInfo.model.js
+++ src/models/medicineInfo.model.js
@@ -4,13 +4,14 @@ const medicineSchema = mongoose.Schema({
     medicineCategory: String,
     medicineName: String,
     batchNo: String,
+    hsnCode:String,
     expDate: Date,
     quantity: Number,
     availableQuantity: Number,
     unit: String,
     price: Number,
     gst: Number,
-    amount: Number
+    amount: Number,
 });
 
 const medicineInfoSchema = mongoose.Schema({
