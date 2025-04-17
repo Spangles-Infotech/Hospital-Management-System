237a9f3d6ba9dc84648d9af60af7541b07c46eaa
sparkie11 <gautamwise.s@gmail.com>
2025-04-17 16:15:57 +0530

    fetch changes

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetch = axios.create({
     // baseURL:"https://hospital-backend-ke9i.onrender.com/api",
     // baseURL:"https://anakuzhi-asan.onrender.com/api",
-       baseURL: "https://hospital-management-system-eexc.onrender.com/api",
+       baseURL: "https://hospital-management-system-eexc.onrender.com/api",  // final render file prod
     
     // baseURL:"http://localhost:3500/api",
 })
