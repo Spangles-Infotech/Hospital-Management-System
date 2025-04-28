2b5e39765dfbd3fdf4c21708bc4c898e01666b23
sparkie11 <gautamwise.s@gmail.com>
2025-04-28 10:25:18 +0530

    cors corrected version 2

--- src/app.js
+++ src/app.js
@@ -25,22 +25,7 @@ connectDB()
 // app.use(express.json());
 
 const corsOptions = {
-    origin: function(origin, callback) {
-        const allowedOrigins = [
-            process.env.FRONTEND_URL_DEV_1,
-            process.env.FRONTEND_URL_DEV_2,
-            process.env.FRONTEND_URL_PRO_1,
-            process.env.FRONTEND_URL_PRO_2,
-            process.env.FRONTEND_URL_PRO_3,
-            process.env.FRONTEND_URL_PRO_4
-        ].filter(Boolean);
-        
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
