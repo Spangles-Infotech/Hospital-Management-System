d11ff1324583676c2624e9eb75ab6ff37f1cccb2
sparkie11 <gautamwise.s@gmail.com>
2025-07-01 11:06:04 +0530

    backedn cahnges

--- src/controllers/patient.controller.js
+++ src/controllers/patient.controller.js
@@ -56,6 +56,16 @@ const patient = async (req, res, next) => {
   
     try {
       if (req.method === "POST") {
+        const lastPatient = await Patient.findOne().sort({ _id: -1 });
+        let nextPatientIdNum = 1;
+        if (lastPatient && lastPatient.patientId) {
+          const lastIdNum = parseInt(lastPatient.patientId.replace('PAT-', ''));
+          if (!isNaN(lastIdNum)) {
+            nextPatientIdNum = lastIdNum + 1;
+          }
+        }
+        const newPatientId = `PAT-${String(nextPatientIdNum).padStart(3, '0')}`;
+        req.body.patientId = newPatientId;
         await Patient.create({ ...req.body, additionalInfo });
         return sendMessage(res, 201, "Patient Created Successfully");
       }
@@ -152,21 +162,4 @@ const getPatientInfo = async(req, res, next)=>{
     }
 }
 
-const getNextPatientId = async (req, res, next) => {
-    try {
-      const lastPatient = await Patient.findOne().sort({ _id: -1 });
-      let nextPatientIdNum = 1;
-      if (lastPatient && lastPatient.patientId) {
-        const lastIdNum = parseInt(lastPatient.patientId.replace('PAT-', ''));
-        if (!isNaN(lastIdNum)) {
-          nextPatientIdNum = lastIdNum + 1;
-        }
-      }
-      const newPatientId = `PAT-${String(nextPatientIdNum).padStart(3, '0')}`;
-      return sendMessage(res, 200, "Next Patient ID fetched Successfully", { patientId: newPatientId });
-    } catch (error) {
-      next(error);
-    }
-  };
-
-module.exports = {patient, getPatientInfo, getNextPatientId}
+module.exports = {patient, getPatientInfo}
--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {patient, getPatientInfo, getNextPatientId} = require("../controllers/patient.controller")
+const {patient, getPatientInfo} = require("../controllers/patient.controller")
 const patientRouter = express.Router()
 
 patientRouter.get("/get-all-patient", patient)
@@ -7,6 +7,5 @@ patientRouter.get("/get-patient/:patientId", patient)
 patientRouter.get("/get-patient-info", getPatientInfo)
 patientRouter.post("/add-patient", patient)
 patientRouter.put("/update-patient/:patientId", patient)
-patientRouter.get("/get-next-patient-id", getNextPatientId)
 
 module.exports = patientRouter
