a6c2410b5cc361e973f6ad7d5250ee2cece325c1
sparkie11 <gautamwise.s@gmail.com>
2025-08-27 01:12:32 +0530

    origin changes

--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -4,7 +4,7 @@ const { addTag, getTags, deleteTag } = require('../controllers/tag.controller');
 const tagrouter = express.Router();
 
 // tagrouter.post('/add-tag', addTag);
-// tagrouter.get('/get-tags', getTags);
+tagrouter.get('/get-tags', getTags);
 tagrouter.delete('/delete-tag/:name', deleteTag);
 
 // module.exports = router;
