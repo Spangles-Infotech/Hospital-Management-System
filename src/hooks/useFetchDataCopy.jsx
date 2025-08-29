cf0d19e8240b35aa0fb2dc5873a5e93d96aee15e
sparkie11 <gautamwise.s@gmail.com>
2025-08-29 15:04:49 +0530

    front

---
+++ src/hooks/useFetchDataCopy.jsx
@@ -0,0 +1,37 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { fetch } from '../api/fetch'
+
+export const useFetchDataCopy = (baseUrl, query) => {
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [data, setData] = useState([])
+    const [total, setTotal] = useState(0)
+    const [error, setError] = useState(null)
+
+    const url = query ? `${baseUrl}?${query}` : baseUrl ;
+
+    const fetchData = useCallback(async () => {
+        setIsLoading(true);
+        try {
+            const response = await fetch.get(url);
+            // console.log(response,"responseresponse")
+            setData(response.data);
+            setTotal(response.data?.total)
+            setError(null);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [url]);
+
+    useEffect(() => {
+        if(baseUrl !== null){
+            fetchData();
+        }
+    }, [fetchData]);
+
+  return{
+    isLoading, data, error, total, fetchData
+  }
+}
